b5ac80beebdee6b8f1bdd77faeab073b86f191b2
jorzyrockz-crypto <jorzyrockz@gmail.com>
2026-08-12 18:18:14 +0800

    Revert extraction from source files to fix CSS/UI, leaving sync-engine intact

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'paperuss-shell-';
-const CACHE_NAME = `${CACHE_PREFIX}v280-sync-repair`;
+const CACHE_NAME = `${CACHE_PREFIX}v280-sync-repair-2`;
 const APP_SHELL = [
   './',
   './index.html',
